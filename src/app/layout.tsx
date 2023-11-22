b4db550c63c3b424f5c32f04aab8c53a40e14485
Jack O'Hara <jack@jackohara.io>
2023-11-22 22:03:55 +0000

    feat: adding manfiest link to html head

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import NextAuthProvider from "./components/NextAuthProvider";
+import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,6 +18,10 @@ export default async function RootLayout({
 }) {
   return (
     <html lang="en">
+      <Head>
+        <link rel="manifest" href="manifest.json" />
+      </Head>
+
       <body
         className={`${inter.className} bg-primary-bg min-h-screen text-gray-300`}
       >
